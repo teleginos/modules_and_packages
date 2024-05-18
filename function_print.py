17c142d8b7dd3e6b310c5a9514f7ba59a2c2659b
Telegin Oleg <teleginos@mail.ru>
2024-05-18 13:04:29 +0300

    add function_print.py

---
+++ function_print.py
@@ -0,0 +1,6 @@
+def print_hello_world():
+    print('Hello world')
+
+
+def print_hello_world_2():
+    print('Hello world again')
---
+++ main.py
@@ -0,0 +1,4 @@
+import function_print
+
+function_print.print_hello_world()
+function_print.print_hello_world_2()
